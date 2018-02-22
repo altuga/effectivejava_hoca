cd3900a4d2daf5a9bd64b5295efd3460810ffe0e
altuga@gmail.com <upux1234>
2018-02-22 09:39:55 +0300

    Lambda fonsiyonu eklendi

---
+++ src/main/java/org/effectivejava/examples/lambda/Order.java
@@ -0,0 +1,18 @@
+package org.effectivejava.examples.lambda;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import static java.util.Comparator.comparingInt;
+
+public class Order {
+
+
+    List<String> words = Arrays.asList("Merhaba", "Dunya");
+
+
+    //TODO 1 - words listesindekileri kelime uzunluklarina gore siralayiniz
+
+}
