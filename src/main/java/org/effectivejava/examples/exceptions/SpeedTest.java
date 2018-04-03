5cce45180bcf16658b40d118a29c0734c3cb933e
altuga@gmail.com <upux1234>
2018-04-03 12:20:58 +0300

    MonthUtil eklendi

---
+++ src/main/java/org/effectivejava/examples/exceptions/SpeedTest.java
@@ -0,0 +1,33 @@
+package org.effectivejava.examples.exceptions;
+
+import java.time.Instant;
+
+public class SpeedTest {
+
+
+
+    public static void main(String[] args) {
+
+
+        long[] myLongArray = new long[10];
+
+
+        long  start = Instant.now().toEpochMilli();
+
+
+            long i = 0;
+            for (int j = 0; j < Integer.MAX_VALUE ; j++) {
+
+                    i += j + myLongArray[j];
+
+            }
+
+            System.out.println(" value = " + i);
+
+
+
+        long  stop = Instant.now().toEpochMilli();
+
+        System.out.println(" Time " +  (stop - start) );
+    }
+}
