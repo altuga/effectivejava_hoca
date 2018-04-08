267825bed72b37b6c7a2c9331a6d75995390e015
altuga@gmail.com <upux1234>
2018-04-08 23:21:04 +0300

    fixed

--- src/main/java/org/effectivejava/examples/chapter06/item32/kodcu/fixed/Text.java
+++ src/main/java/org/effectivejava/examples/chapter06/item32/kodcu/fixed/Text.java
@@ -11,12 +11,14 @@ public class Text {
 
 	// Any Set could be passed in, but EnumSet is clearly best
 	public void applyStyles(Set<Style> styles) {
-		// Body goes here
+		for (Style style : styles) {
+			System.out.println(" " + style);
+		}
 	}
 
 	// Sample use
 	public static void main(String[] args) {
 		Text text = new Text();
-		text.applyStyles(EnumSet.of(Style.BOLD, Style.ITALIC));
+		text.applyStyles(EnumSet.of(Style.BOLD, Style.BOLD, Style.ITALIC));
 	}
 }
