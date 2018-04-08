fa45c7dfa42ee35e44089408eb0e785ce3eb4e70
altuga@gmail.com <upux1234>
2018-04-09 02:34:57 +0300

    IOException added

--- src/main/java/org/effectivejava/examples/allinone/StaticFactoryExamples.java
+++ src/main/java/org/effectivejava/examples/allinone/StaticFactoryExamples.java
@@ -1,16 +1,32 @@
 // ITEM 1
 package org.effectivejava.examples.allinone;
 
+import org.effectivejava.examples.chapter04.item18.AbstractMapEntry;
+
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.lang.reflect.Array;
 import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.Instant;
+import java.util.AbstractCollection;
 import java.util.Date;
 import java.util.EnumSet;
 
 public class StaticFactoryExamples {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+
+        /**
+         * Often static factories are preferable
+         *
+         * so avoid the reflex to provide
+         * public constructors without first considering
+         * static factories.
+         */
         Date d = Date.from(Instant.now());
 
        // Set<Rank> faceCards = EnumSet.of(JACK, QUEEN, KING);
@@ -19,5 +35,9 @@ public class StaticFactoryExamples {
 
         Date[] dates = (Date[]) Array.newInstance(Date.class, 1);
 
+        Path logFile = Paths.get("/temp/logs/app.log");
+        BufferedReader br = Files.newBufferedReader(logFile);
+
     }
 }
+
