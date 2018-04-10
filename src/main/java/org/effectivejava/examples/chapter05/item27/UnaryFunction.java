424ade8b3ce589de7fc1862ade8fe243a02df669
altuga@gmail.com <upux1234>
2018-04-10 22:30:23 +0300

    temizlik

--- src/main/java/org/effectivejava/examples/chapter05/item27/UnaryFunction.java
+++
@@ -1,6 +0,0 @@
-// Unary function - page 131
-package org.effectivejava.examples.chapter05.item27;
-
-public interface UnaryFunction<T> {
-	T apply(T arg);
-}
--- src/main/java/org/effectivejava/examples/chapter05/item28/EmptyStackException.java
+++
@@ -1,4 +0,0 @@
-package org.effectivejava.examples.chapter05.item28;
-
-public class EmptyStackException extends RuntimeException {
-}
