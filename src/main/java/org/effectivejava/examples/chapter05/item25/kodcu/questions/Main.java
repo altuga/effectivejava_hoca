b6b1336b8dd2aa897a3a8187f44fcb4f6e1e4865
altuga@gmail.com <upux1234>
2018-06-29 10:42:28 +0300

    Chapter 5 - Prefer lists to arrays için eklendi

---
+++ src/main/java/org/effectivejava/examples/chapter05/item25/kodcu/questions/Main.java
@@ -0,0 +1,19 @@
+package org.effectivejava.examples.chapter05.item25.kodcu.questions;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Object[] objectArray = new Long[1];
+        //objectArray[0] = "Selam";
+
+        //List<Long> objectList = new ArrayList<Long>();
+        //objectList.add("selam");
+
+
+    }
+}
