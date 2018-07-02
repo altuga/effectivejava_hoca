d25f3d21f8407ee250e3082500817d9a6001aaaa
altuga@gmail.com <upux1234>
2018-07-03 01:48:02 +0300

    refactor edildi

--- src/main/java/org/effectivejava/examples/chapter06/item32/kodcu/questions/Metin.java
+++ src/main/java/org/effectivejava/examples/chapter06/item32/kodcu/questions/Metin.java
@@ -3,18 +3,18 @@ package org.effectivejava.examples.chapter06.item32.kodcu.questions;
 
 public class Metin {
 	public static final int BOLD = 1 ;
-	public static final int ITALIC = 1 ;
-	public static final int UNDERLINE = 1;
-	public static final int STRIKETHROUGH = 1;
+	public static final int ITALIC = 2 ;
+	public static final int UNDERLINE = 3;
+	public static final int STRIKETHROUGH = 4;
 
 
-	public void applyStyles(int styles) {
+	public void secilenStiliUygula(int stiller) {
 		// Body goes here
 	}
 
 	// Sample use
 	public static void main(String[] args) {
 		Metin metin = new Metin();
-		metin.applyStyles(BOLD);
+		metin.secilenStiliUygula(BOLD);
 	}
 }
